9c485e5daf7512713360be2bb9fee1f196845402
Martijn de Vos <mdmartijndevos@gmail.com>
2017-11-28 16:26:26 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='A description.',
     packages=find_packages(),
-    package_data={'lib2to3': ['*']},
+    package_data={'': ['*']},
     include_package_data=True,
     install_requires=[],
 )
